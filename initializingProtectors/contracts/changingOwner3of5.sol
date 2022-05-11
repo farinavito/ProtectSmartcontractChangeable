907ec58d2009f50ce85345cff9d42f51f5795e93
Vito <farinavito@gmail.com>
2022-05-11 11:52:04 +0200

    added comments to the code

--- initializingProtectors/contracts/changingOwner3of5.sol
+++ initializingProtectors/contracts/changingOwner3of5.sol
@@ -37,15 +37,18 @@ contract ChangingOwner {
         address _protector4, 
         address _protector5 
     ){
+        //storing the smartcontract's owner
         newOwner = _smartContractOwner;
         smartcontractOwner[_smartContractOwner] = true;
 
+        //Storing all protectors
         allprotectorsaddresses.push(_protector1);
         allprotectorsaddresses.push(_protector2);
         allprotectorsaddresses.push(_protector3);
         allprotectorsaddresses.push(_protector4);
         allprotectorsaddresses.push(_protector5);
 
+    //initialize the protectors
     for (uint8 i = 1; i <= 5; i++){
             candidates[_smartContractOwner] += 1;
             alreadyVoted[allprotectorsaddresses[i - 1]][_smartContractOwner] = true;
@@ -67,14 +70,16 @@ contract ChangingOwner {
 
     /// @notice Changing the smartContractOwner
     function changeOwner(address _nextInline) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //checking if the _nextInLine has enough votes
         require(candidates[_nextInline] >= 3, "Not enough protectors agree with this address");
-        //old values to zero and false
+        //changing old values of protectors to zero and false
         candidates[newOwner] = 0;
         for (uint8 i = 0; i < 5; i++){
             alreadyVoted[allprotectorsaddresses[i]][newOwner] = false;
         }
-        //deleting the previous owner
+        //changing the previous owner to false
         smartcontractOwner[newOwner] = false;
         //change the owner
         smartcontractOwner[_nextInline] = true;
@@ -83,17 +88,25 @@ contract ChangingOwner {
 
     /// @notice Voting for candidates by protectors
     function voteForOwnerCandidate(address _nextInLine) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //checking if the caller hasn't already voted for _nextInLine
         require(alreadyVoted[msg.sender][_nextInLine] == false, "You have entered your vote");
+        //declaring that the caller has already voted for _nextInLine
         alreadyVoted[msg.sender][_nextInLine] = true;
+        //increasing the number of votes for _nextInLine
         candidates[_nextInLine] += 1;
     }
 
     /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
     function removeVoteForOwnerCandidate(address _nextInLine) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //checking if the caller has already voted for _nextInLine
         require(alreadyVoted[msg.sender][_nextInLine] == true, "You haven't voted for this address");
+        //declaring that the caller hasn't already voted for _nextInLine
         alreadyVoted[msg.sender][_nextInLine] = false;
+        //decreasing the number of votes for _nextInLine
         candidates[_nextInLine] -= 1;
     }
   
@@ -106,34 +119,46 @@ contract ChangingOwner {
 
     /// @notice Changing the protector's address
     function changeProtector(address _newAddress, address _oldAddress) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //storing the old protector's id
         uint256 idOfOurAddress = checkWhichProtector(_oldAddress);
+        //checking if the _nextInLine has enough votes
         require(candidatesChange[_newAddress] >= 3, "Not enough protectors agree with this address");
-        //old values to zero and false
+        //changing the old values of protectors to zero and false
         candidates[_oldAddress] = 0;
         for (uint8 i = 0; i < 5; i++){
             alreadyVotedChange[allprotectorsaddresses[i]][_oldAddress] = false;
         }
-        //change the protector
+        //change the protector to _newAddress
         allprotectorsaddresses[idOfOurAddress] = _newAddress;
     }
 
     /// @notice Voting for candidates by protectors
     function voteForProtectorCandidate(address _nextInLine) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //checking if the caller hasn't already voted for _nextInLine
         require(alreadyVotedChange[msg.sender][_nextInLine] == false, "You have entered your vote");
+        //checking if _nextInLine address already exists as a protector
         for (uint8 i = 0; i < 5; i++){
             require(allprotectorsaddresses[i] != _nextInLine, "This protector already exsists");
         }
+        //declaring that the caller has already voted for _nextInLine
         alreadyVotedChange[msg.sender][_nextInLine] = true;
+        //increasing the number of votes for _nextInLine
         candidatesChange[_nextInLine] += 1;
     }
 
     /// @notice remove vote by the protector from previously voted protectorWaitingToBeOwner
     function removeVoteForProtectorCandidate(address _nextInLine) external {
+        //checking if the caller is one of the protectors
         checkWhichProtector(msg.sender);
+        //checking if the caller has already voted for _nextInLine
         require(alreadyVotedChange[msg.sender][_nextInLine] == true, "You haven't voted for this address");
+        //declaring that the caller hasn't already voted for _nextInLine
         alreadyVotedChange[msg.sender][_nextInLine] = false;
+        //decreasing the number of votes for _nextInLine
         candidatesChange[_nextInLine] -= 1;
     }
 }
